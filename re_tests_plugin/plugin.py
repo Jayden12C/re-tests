017e6c46600f36812e6ebd36426f76fbeb76faaa
Kashin Nikita <nikita.kashin@red-soft.ru>
2023-04-04 15:28:40 +0300

    Some change in fixture "open_connection"
    Change version

--- re_tests_plugin/plugin.py
+++ re_tests_plugin/plugin.py
@@ -5,15 +5,12 @@ import time
 @pytest.fixture()
 def open_connection():
     lackey.App.focus("Red Expert")
-    lackey.rightClick("files/images/tree_name_of_conn_blue.png")
-    lackey.click("files/images/tree_db_info.png")
-    lackey.click("files/images/init_db_browser.png")
-    lackey.click("files/images/bt_conn.png")
+    lackey.doubleClick("files/images/tree_name_of_conn_blue.png")
     while lackey.exists("files/images/tree_name_of_open_conn.png") == None:
+        print(lackey.exists("files/images/tree_name_of_open_conn.png"))
         time.sleep(1)
     yield
-    lackey.rightClick("files/images/tree_name_of_open_conn.png")
-    lackey.click("files/images/tree_close_conn.png")
+    lackey.doubleClick("files/images/tree_name_of_open_conn.png")
     
 @pytest.fixture(scope='session', autouse=True)
 def create_connection():
@@ -27,6 +24,4 @@ def create_connection():
     lackey.type("sysdba")
     lackey.click("files/images/text_input_password.png")
     lackey.type("masterkey")
-    lackey.click("files/images/chb_store_pass.png")
-
-    
+    lackey.click("files/images/chb_store_pass.png")    
