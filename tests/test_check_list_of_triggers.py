9654c020c5070ec8ccd889c9dfdc63dbda9a08f7
Kashin Nikita <nikita.kashin@red-soft.ru>
2023-04-04 15:32:11 +0300

    Add test on check list of trigger in table tab "Triggers"
    Delete useless file

---
+++ tests/test_check_list_of_triggers.py
@@ -0,0 +1,18 @@
+import lackey
+from re_tests_plugin import *
+
+def test_1(open_connection):
+    lackey.click("files/images/tree_plus.png")
+    lackey.click("files/images/tree_plus.png")
+    all_plus = lackey.findAll("files/images/tree_plus.png")
+    lackey.click(list(all_plus)[3])
+    lackey.doubleClick("files/images/tree_table_name_EMPLOYEE.png")
+    lackey.click("files/images/tab_triggers.png")
+    lackey.doubleClick("files/images/trigger_name.png")
+    time.sleep(2)
+    lackey.type("{ESC}")
+    result = lackey.find("files/images/trigger_name.png")
+    lackey.rightClick("files/images/tab_table_EMPLOYEE_blue.png")
+    lackey.click("files/images/bt_tab_close_all.png")
+    assert result != None, "List of triggers is empty"
+    
