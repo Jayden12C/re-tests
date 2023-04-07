467a748ea9ddf64ae933a5bff5d15fd285d0e31b
Kashin Nikita <nikita.kashin@red-soft.ru>
2023-04-07 12:56:54 +0300

    Add set settings befor test session

--- re_tests_plugin/plugin.py
+++ re_tests_plugin/plugin.py
@@ -11,10 +11,14 @@ def open_connection():
     else:
         lackey.doubleClick("files/images/tree_name_of_conn.png")
     time.sleep(5)    #waiting time to open connection
+    lackey.click("files/images/tree_name_of_open_conn_blue.png")
     #actions after test:
     yield
-    lackey.doubleClick("files/images/tree_name_of_open_conn.png")
+    if lackey.exists("files/images/tree_name_of_open_conn.png") != None:
+        lackey.doubleClick("files/images/tree_name_of_open_conn.png")
+    else:
+        lackey.doubleClick("files/images/tree_name_of_open_conn_blue.png")
     
-# @pytest.fixture(scope='session', autouse=True)
-# def create_connection():
-#     pass   
+@pytest.fixture(scope='session', autouse=True)
+def create_connection():
+    lackey.SettingsMaster.MinSimilarity = 0.97  
