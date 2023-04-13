b4a28a36eb9515cb6464f36943fbaaee12e899b0
Kashin Nikita <nikita.kashin@red-soft.ru>
2023-04-13 10:38:20 +0300

    Rename from "tip" to "hint"
    Increase waiting time for a hint

--- tests/test_check_hint.py
+++ tests/test_check_hint.py
@@ -5,12 +5,12 @@ def test_1(open_connection):
     move_location = lackey.find("files/images/icon_connected.png").getTarget()
     mouse = lackey.Mouse()
     mouse.move(loc=move_location)
-    time.sleep(0.5)
-    assert lackey.find("files/images/tip_disconnect.png") != None
+    time.sleep(1)
+    assert lackey.find("files/images/hint_disconnect.png") != None
 
 def test_2():
     move_location = lackey.find("files/images/icon_disconnected.png").getTarget()
     mouse = lackey.Mouse()
     mouse.move(loc=move_location)
-    time.sleep(0.5)
-    assert lackey.find("files/images/tip_connect.png") != None
+    time.sleep(1)
+    assert lackey.find("files/images/hint_connect.png") != None
