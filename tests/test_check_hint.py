1e3e63cb9d08f9d779973e1a29dc990cac23dbef
Kashin Nikita <nikita.kashin@red-soft.ru>
2023-04-17 17:28:26 +0300

    Change MinSimilarity for certain tests

--- tests/test_check_hint.py
+++ tests/test_check_hint.py
@@ -2,15 +2,21 @@ import lackey
 from re_tests_plugin import *
 
 def test_1(open_connection):
+    lackey.SettingsMaster.MinSimilarity = 0.7
     move_location = lackey.find("files/images/icon_connected.png").getTarget()
     mouse = lackey.Mouse()
     mouse.move(loc=move_location)
     time.sleep(1)
-    assert lackey.find("files/images/hint_disconnect.png") != None
+    result = lackey.find("files/images/hint_disconnect.png")
+    lackey.SettingsMaster.MinSimilarity = 0.97
+    assert result != None
 
 def test_2():
+    lackey.SettingsMaster.MinSimilarity = 0.7
     move_location = lackey.find("files/images/icon_disconnected.png").getTarget()
     mouse = lackey.Mouse()
     mouse.move(loc=move_location)
     time.sleep(1)
-    assert lackey.find("files/images/hint_connect.png") != None
+    result = lackey.find("files/images/hint_connect.png")
+    lackey.SettingsMaster.MinSimilarity = 0.97
+    assert result != None
