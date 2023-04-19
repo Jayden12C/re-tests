70d32bf97044a687f217df797eae621fd99a3806
Kashin Nikita <nikita.kashin@red-soft.ru>
2023-04-19 11:33:14 +0300

    Use ImagePaths from settings now

--- re_tests_plugin/plugin.py
+++ re_tests_plugin/plugin.py
@@ -1,6 +1,8 @@
 import pytest
 import lackey
 import time
+import platform
+
 
 ADMIN_NAME = 'sysdba'
 ADMIN_PASSWORD = 'masterkey'
@@ -9,20 +11,30 @@ ADMIN_PASSWORD = 'masterkey'
 def open_connection():
     #actions befor test:
     lackey.App.focus("Red Expert")
-    if lackey.exists("files/images/tree_name_of_conn_blue.png") != None:
-        lackey.doubleClick("files/images/tree_name_of_conn_blue.png")
+    if lackey.exists("tree_name_of_conn_blue.png") != None:
+        lackey.doubleClick("tree_name_of_conn_blue.png")
     else:
-        lackey.doubleClick("files/images/tree_name_of_conn.png")
+        lackey.doubleClick("tree_name_of_conn.png")
     time.sleep(2)    #waiting time to open connection
-    lackey.click("files/images/tree_name_of_open_conn_blue.png")
+    lackey.click("tree_name_of_open_conn_blue.png")
     #actions after test:
     yield
-    if lackey.exists("files/images/tree_name_of_open_conn.png") != None:
-        lackey.doubleClick("files/images/tree_name_of_open_conn.png")
+    if lackey.exists("tree_name_of_open_conn.png") != None:
+        lackey.doubleClick("tree_name_of_open_conn.png")
     else:
-        lackey.doubleClick("files/images/tree_name_of_open_conn_blue.png")
+        lackey.doubleClick("tree_name_of_open_conn_blue.png")
     
 @pytest.fixture(scope='session', autouse=True)
 def create_connection():
     lackey.App.focus("Red Expert")
+    
+    image_path = ["files/images/"]
+
+    #code for the future
+    # if platform.system() == "Windows":
+    #     image_path += ["Windows/"]
+    # else:
+    #     image_path += ["Linux/"]
+    
+    lackey.SettingsMaster.ImagePaths = image_path
     lackey.SettingsMaster.MinSimilarity = 0.97
