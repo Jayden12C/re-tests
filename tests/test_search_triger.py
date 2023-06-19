2124c319d939face60d03987f9933da9cb5cc3d7
Jeyden12 <janypanov@gmail.com>
2023-06-19 17:26:23 +0300

    test search trigger

--- tests/test_search_triger.py
+++ tests/test_search_triger.py
@@ -5,7 +5,6 @@ from re_tests_plugin import *
 
 def test_search_triger(open_connection):
     triger = "alter trigger fts$trig_22 sql security definer"
-    lackey.click("tree_plus.png")
     lackey.click("tab_query_editor_text.png")
     lackey.type(triger)
     lackey.click("icon_execute_query.png")
